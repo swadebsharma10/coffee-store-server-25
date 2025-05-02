2b78edcd78c074e0cd04d345f25c64bd3ca6ca65
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-05-03 02:38:26 +0600

    create user delete and single user api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,7 +13,6 @@ app.get("/", (req, res) => {
   res.send("Coffee server running!");
 });
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fikwith.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,7 +21,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,6 +30,43 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDBNew").collection("coffees");
+    const usersCollection = client.db("coffeeDBNew").collection("users");
+
+    // user related Api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users", async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const logInfo = req.body;
+
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body.lastLoginTime
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    });
+
+    // Coffee related Api
 
     app.get("/coffee", async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -52,16 +87,16 @@ async function run() {
       res.send(result);
     });
 
-     // Delete a Coffee
-     app.delete("/coffee/:id", async (req, res) => {
+    // Delete a Coffee
+    app.delete("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     });
 
-     // update a coffee item
-     app.put("/coffee/:id", async (req, res) => {
+    // update a coffee item
+    app.put("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const updatedCoffee = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -86,21 +121,16 @@ async function run() {
       res.send(result);
     });
 
-
-    
-
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`Coffee server listening on port ${port}`);
